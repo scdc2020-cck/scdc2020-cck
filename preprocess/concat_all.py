23cf2a6a3b2ead826c5300983131de972f202872
cym-2 <cym.7172@gmail.com>
2020-11-09 15:06:05 +0900

    Update concat_all.py

--- preprocess/concat_all.py
+++ preprocess/concat_all.py
@@ -11,23 +11,33 @@ import os
 # In[ ]:
 
 
-d = 'image31'
-n = '0'
+#all_preprocess_0 ~ all_preprocess_9 를 합칠 때 실행
 
 df = pd.DataFrame()
 df_list = list()
 
 for i in range(10):
-    fname = os.path.join(os.getcwd(), f"{d +'_'+n+'_'+'all_'+str(i)}.csv")
+    fname = os.path.join(os.getcwd(), f"{'all_preprocess_'+str(i)}.csv")
     dff = pd.read_csv(fname, index_col=0)
     df_list.append(dff)
     
 df = pd.concat(df_list, ignore_index=True)
-df.to_csv(d +'_'+n+'_'+'all.csv')
+df.to_csv('all_preprocess.csv')
 
 
 # In[ ]:
 
 
+#quiz_preprocess_0 ~ quiz_preprocess_9 를 합칠 때 실행
 
+df = pd.DataFrame()
+df_list = list()
+
+for i in range(10):
+    fname = os.path.join(os.getcwd(), f"{'quiz_preprocess_'+str(i)}.csv")
+    dff = pd.read_csv(fname, index_col=0)
+    df_list.append(dff)
+    
+df = pd.concat(df_list, ignore_index=True)
+df.to_csv('quiz_preprocess.csv')
 
